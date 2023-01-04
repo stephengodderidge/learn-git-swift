78eb754c74389d4f00383fbfc3fa707da50ef200
Stephen Godderidge <steve.godderidge@gmail.com>
2023-01-03 22:21:24 -0800

    Initial Commit

---
+++ learn-git-xcode/learn_git_xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_git_xcodeApp.swift
+//  learn-git-xcode
+//
+//  Created by Stephen Godderidge on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct learn_git_xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
